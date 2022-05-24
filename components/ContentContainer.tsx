a918bb4730b7eea9157f394b830c42236d625147
Oscar Coniel <oscar@coniel.com>
2022-05-24 15:14:59 +0300

    [components] add ContentContainer

---
+++ components/ContentContainer.tsx
@@ -0,0 +1,11 @@
+import { styled } from '../stitches.config';
+
+export const ContentContainer = styled('div', {
+  maxWidth: 1310,
+  paddingRight: 80,
+  paddingLeft: 80,
+  width: '100%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  overflow: 'visible',
+});
